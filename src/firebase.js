34b1d58cc3bf0529a443f0368c4a7787cc8f3dc1
satkar2001 <satkaracharya883@gmail.com>
2022-12-28 22:28:17 +0530

    firebase changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDArVa0UjpW7PqmrsMWcjsS82QvezLCmg4",
-  authDomain: "quora-clone-mern.firebaseapp.com",
-  projectId: "quora-clone-mern",
-  storageBucket: "quora-clone-mern.appspot.com",
-  messagingSenderId: "182383235813",
-  appId: "1:182383235813:web:fd9e0c34fcc0a4f07c7a0c",
-  measurementId: "G-62KKTN74PZ",
+  apiKey: "AIzaSyD1VgcoA-toapB9rnn-XbSvxblfzOPTrg0",
+  authDomain: "askvit-da6aa.firebaseapp.com",
+  projectId: "askvit-da6aa",
+  storageBucket: "askvit-da6aa.appspot.com",
+  messagingSenderId: "465966102115",
+  appId: "1:465966102115:web:50225014ecebdb85086a56",
+  measurementId: "G-4306LJ6QX3"
 };
 
 // Initialize Firebase
